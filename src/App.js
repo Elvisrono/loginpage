92c4f87d2c384a63f39605e540d59f2aedb0e523
ELVIS RONO <Elvis>
2024-05-10 16:32:32 +0300

    testing api json data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => setUsers(data))
+    .catch(error => console.error('Error fetching users:', error));
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            <h2>{user.name}</h2>
+            <p>Email: {user.email}</p>
+            <p>Adress: {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</p>
+            <p>Phone: {user.phone}</p>
+            <p>Website: {user.website}</p>
+            <p>Company: {user.company.name}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
